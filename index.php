3d1ebdfd45f5883bc6c3dcee1bca888288a7051e
Vic1506 <78886523+Vic1506@users.noreply.github.com>
2021-08-26 18:42:06 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Info personal vic</title>
+</head>
+<body>
+    <h1>MATERIA DE PRACTICA DE DESARROLLO 2</h1>
+    <tr> 
+    <img src="foto.jpeg" height="25%" width="25%" alt=""> <br>
+    <td> ~Yo con mis mejores amigas <3 <br></td>
+    <br>
+    </tr>
+    <tr> 
+    <img src="foto2.jpeg" height="25%" width="25%" alt=""> <br>
+    <td> ~Yo actualmente en cuarentena :( <br></td>
+    <br>
+    </tr>
+    <tr> 
+          <td> ~Nombre: Victor Hugo Felix Valenzuela <br></td>
+           </tr> <br>
+
+    <tr>
+       <td> ~Lugar de nacimiento: Hermosillo, Sonora <br></td>
+    </tr>
+    <tr> <br>
+       <td> ~Estudios: Actualmente estudio en la Universidad de 
+        Sonora la carrera de ingenieria en sistemas de informacion.</td>
+<br>
+    </tr>
+    <tr> <br>
+       <td> ~Experiencia: Solamente academica. <br></td>
+    </tr>
+    <tr> <br>
+       <td> ~Logros personales: Soy una persona con buena destreza en los deportes
+        principalmente en TaeKwonDo siendo cinta negra, ganando algunos torneos y con un gran gusto por el 
+        ajedrez ganando tambien algunos torneos locales. <br>
+        </td>
+    </tr>
+    <tr> <br>
+       <td> ~Gustos personales: Me gusta mucho la musica (Billie Eilish, Queen y Eminem principalmente... y ahora BTS),
+        los deportes principalmente futbol y ajdrez y tambien tengo una gran aficion al cine y las series.
+        Mi favorita es Game of Thrones y mi pelicula The imitation game. Durante esta cuarentena
+        le he agarrado un gran gusto a los idiomas (ando aprendiendo) y quiero aprender muchos a lo 
+        largo de mi vida.
+        </td>
+    </tr>
+     <ul>
+         <br/>
+        <li><a href="https://vicfelix.000webhostapp.com/index.php">Mi pagina en 000Webhost</a></li>
+        <li><a href="https://vicfelix.victorfelix3.repl.co">Mi pagina en Replit</a></li>
+    </ul>
+    </tr>
+
+    
+    
+</body>
+</html>
